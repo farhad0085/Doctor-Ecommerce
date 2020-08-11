a90f9e3409b929c82602becf601c4c65eb22c586
Farhad Hossain <farhadhossain0085@gmail.com>
2020-08-12 02:29:41 +0600

    patient doctor option removed from signup form

--- run.py
+++ run.py
@@ -1,17 +1,8 @@
 from app import app
 
-
 @app.route('/')
 def home():
 	return "home found"
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-	app.run(debug=True, host="127.0.0.1", port=5000)
-=======
-<<<<<<< HEAD
-	app.run(debug=True,host="127.0.0.1", port=5000)
-=======
 	app.run(debug=True)
->>>>>>> 8e6099b463d88607eaca1bcffe6ec56ddcc20bd8
->>>>>>> 2f8683a9a8856bab709899ef63ffd39e31020ea9
